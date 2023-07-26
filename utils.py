36a23e895f06f9b566f6a332fa9c7c96b79455d5
Kevin Giesen <giesen.kevin@yahoo.de>
2023-07-26 16:53:03 +0200

    comments added

--- utils.py
+++ utils.py
@@ -13,34 +13,48 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 # Function which finds the lines where a players name is contained
 def find_lines_with_player(dataframe, playerlist, n_lines = 0):
+    """
+    This function transform article data into just the lines where a playername appears. 
     
-    # create empty df 
+    Parameters:
+        dataframe: DataFrame which contains the whole article data in the column "data"
+        playerlist: list of all players for which the lines should be conducted
+        n_lines: number of lines following the line where the name appears which also should be conducted
+    
+    Returns:
+        DataFrame with the sentences where the playername appears as data column
+    """
+    
+    # Create empty df 
     df_complete = pd.DataFrame()
 
-    # iterating over all players
+    # Iterating over all players
     for player in playerlist:
 
-        # get players first and last_name to include them in later sentence checks
+        # Get players first and last_name to include them in later sentence checks
         player_first_name, player_last_name = player.split()
 
-        # just select player indiviual data
+        # Just select player indiviual data
         df_player = dataframe[dataframe["player"] == player]
         df_player = df_player.reset_index(drop=True)
 
-        # iterate over all data for the player
+        # Iterate over all data for the player
         for i in range(len(df_player)):
 
-            # get the current record
+            # Get the current record
             current_line = df_player['data'].iloc[i]
-            # split up the records in lines
+
+            # Split up the records in lines
             lines = current_line.split('\\n')
-            # create an empty string
+
+            # Create an empty string
             new_string = ''
 
             line_counter = 0
-            # iterate over all lines in the record
+            # Iterate over all lines in the record
             for line in lines:
-                # if the playername can be found in the line add the line to the string
+
+                # If the playername can be found in the line add the line to the string
                 if line.find(player) != -1:
                     new_string = new_string + line + " "
                     if line_counter <= 0:
@@ -60,17 +74,27 @@ def find_lines_with_player(dataframe, playerlist, n_lines = 0):
                     new_string = new_string + line + " "
                     line_counter = line_counter-1
         
-            # switch the previos record against the newly created string 
+            # Switch the previos record against the newly created string 
             df_player['data'].iloc[i] = new_string
 
-        # add the new data to the Dataframe and return
+        # Add the new data to the Dataframe and return
         df_complete = pd.concat([df_complete, df_player], axis=0)
         
     return df_complete
 
-def remove_similar_rows(column_as_df, df, threshold=0.9):
-    ''' The old Function of removing similiarities is deleting allduplicate articles'''
 
+def remove_similar_rows(column_as_df, df, threshold=0.9):
+    """
+    This function deletes similiar data rows.
+    
+    Parameters:
+        column_as_df: data out of a DataFrame where the similiar rows should be removed
+        df: corresponding DataFrame
+        threshold: integer with degree of row similiarity. default is deletion of rows which are 90% similiar.
+    
+    Returns:
+        DataFrame with dropped rows.
+    """
     # Compute similarity scores for each pair of rows
     similarity_scores = {}
     for i, row in column_as_df.iterrows():
@@ -90,16 +114,29 @@ def remove_similar_rows(column_as_df, df, threshold=0.9):
     # Remove rows and return modified DataFrame
     return df.drop(rows_to_remove)
 
-def remove_similar_rows_per_player(df, playerlist, threshold=0.9):
-    '''The procedure of deleting similiar articles needs to be done by each player because if an article writes about 
-    # e.g. two players we want to keep it for both of the players'''
 
-    # define empty df which will be returned in the end
+
+def remove_similar_rows_per_player(df, playerlist, threshold=0.9):
+    """
+    This function deletes similiar data rows for each player individually. 
+    The procedure deletion of similiar articles needs to be done for each player individually, 
+    because articles which include information for multiple players should then also be used for multiple players.
+    
+    Parameters:
+        df: Dataframe which containes the columns: data and player as an input. The column data relates to news articles.
+        playerlist: list of all players for which rows should be removed
+        threshold: integer with degree of row similiarity. default is deletion of rows which are 90% similiar.
+    
+    Returns:
+        DataFrame with dropped rows.
+    """
+
+    # Define empty df which will be returned in the end
     df_complete = pd.DataFrame()
 
     for player in playerlist:
         
-        # create the df for the player
+        # Create the df for the player
         df_player = df[df["player"] == player]
         df_player = df_player.reset_index(drop=True)
         column_as_df = pd.DataFrame(df_player['data'])
@@ -116,7 +153,7 @@ def remove_similar_rows_per_player(df, playerlist, threshold=0.9):
                 if score >= threshold:
                     similarity_scores[(i, j)] = score
         
-        # Identify rows to remove
+        # Identify rows to remove 
         rows_to_remove = []
         for (i, j), score in similarity_scores.items():
             if i not in rows_to_remove and j not in rows_to_remove:
@@ -126,48 +163,93 @@ def remove_similar_rows_per_player(df, playerlist, threshold=0.9):
         df_player = df_player.drop(rows_to_remove)
         df_complete = pd.concat([df_complete, df_player], axis=0)
 
-        #return modified DataFrame
+        #Return modified DataFrame
     return df_complete
 
+
 def map_emoji_to_description(emoji_text, language): 
+    """
+    This function transforms emojis into text
+    
+    Parameters:
+        emoji_text: string which contains emojis
+        language: language in which the text is written and to which the emojis should be mapped
+    
+    Returns:
+        string with text instead of emojis
+    """
     emoji_description = emoji.demojize(emoji_text, language=language)
     return emoji_description
 
+
 def translate_emojis(text, language):
+    """
+    This function transforms text which contains emojis into text without emojis 
+    
+    Parameters:
+        text: string which contains emojis
+        language: language in which the text is written and to which the emojis should be mapped
+    
+    Returns:
+        string with text instead of emojis
+    """
     return re.sub(r'[\U0001F000-\U0001F999]', lambda match: map_emoji_to_description(match.group(), language=language), text)
 
+
 def remove_accents(text):
+    """
+    This function removes accents from text
+    
+    Parameters:
+        text: string which contains accents
+    
+    Returns:
+        string without accents
+    """
     return unidecode(text)
 
+
 def remove_stopwords_from_text(text, stopwords_list_per_language):
+    """
+    This function removes stopwords from text
+    
+    Parameters:
+        text: string which contains stopwords
+        stopwords_list_per_language: stopwords which should be deleted
+    
+    Returns:
+        string without stopwords
+    """
     return remove_stopwords(text, stopwords=stopwords_list_per_language)
 
+
+
 def del_patterns(df_line, pattern):
-    '''
-    Function which takes an input and deletes defined text pattern 
-    '''
-    # split up the records in lines
+    """
+    This function removes defined patterns from a list 
+    
+    Parameters:
+        df_line: string which contains pattern
+        pattern: pattern which should be delted
+    
+    Returns:
+        string without stopwords
+    """
+
+    # Split up the records in lines
     lines = df_line.split('\\n')
-    '''
-    lines = df_line.split('\n')  # Split at newline characters first
-
-    split_lines = []
-    for line in lines:
-        split_lines.extend(line.split('\\t'))  # Split each line at tab characters
-
-    '''
 
     if len(lines)>1:
 
-        # create an empty string
+        # Create an empty string
         new_string = ''
 
-        # iterating over the lines 
+        # Iterating over the lines 
         for line in lines:
         
-        # set deleting to False first
+        # Set deleting to False first
             deleting = False
-            # check if a pattern word is included in the line and set deleting to True if so 
+            # Check if a pattern word is included in the line and set deleting to True if so 
             for word in pattern:
                 if deleting == True:
                     break
@@ -176,39 +258,47 @@ def del_patterns(df_line, pattern):
                 else:
                     deleting = False
             
-            # if the setence should not be delete it add it to the string  
+            # If the setence should not be delete it add it to the string  
             if deleting == False:
                 new_string = new_string + line + '\\n '
 
     else:
         new_string = df_line
-    # return the string 
+    # Return the string 
     return new_string
 
 
 def word_frequency_per_player(df, playerlist):
-    '''
-    Function which returns the frequncy of words in articles for all players 
-    '''
-     # define empty df which will be returned in the end
+    """
+    This function returns the word freqencies for each players data. 
+    
+    Parameters:
+        df: DataFrame with the data in the column 'data'
+        playerlist: list of players for which the word frequencies should be conducted
+    
+    Returns:
+        DataFrame with all players and corresponding word frequencies
+    """
+     # Define empty df which will be returned in the end
     df_complete = pd.DataFrame()
 
+    # Iterate over players
     for player in playerlist:
 
-         # create the df for the player
+         # Create the df for the player
         df_player = df[df["player"] == player]
         df_player = df_player.dropna(subset=['data'])
         df_player = df_player.reset_index(drop=True)
 
-        # create a stemmed data corpus
+        # Create a stemmed data corpus
         df_player['stemmed_data'] = df_player['data'].apply(stem_text)
         data_stem = df_player['data'].apply(stem_text)
         data = data_stem.tolist()
 
-        # create a corpus
+        # Create a corpus
         corpus_gen=[doc.split() for doc in data]
 
-        # Assume `corpus` is a preprocessed corpus
+        # Store corpus Dicitonary
         id2word = Dictionary(corpus_gen)
 
         # Filter out rare and common words
@@ -224,17 +314,61 @@ def word_frequency_per_player(df, playerlist):
             df_frequencies.loc[i]= [id2word[feature],frequency, player]
             i = i+1
 
+        # Sort the frequencies descanding
         df_frequencies = df_frequencies.sort_values('Frequency', ascending=False)
 
+        # Concatenate to df_complete
         df_complete = pd.concat([df_complete, df_frequencies], axis=0)
 
+    # return df_complete
     return df_complete
 
 
+def print_player_freq(df_freq):
+    """
+    This function prints out the 20 most common words for each players data. 
+    
+    Parameters:
+        df_freq: DataFrame with the word frequncy data conducted with the "word_frequency_per_player" function
+    
+    Returns:
+        DataFrame with the 20 most common words per player
+    """
+    # Create empty df
+    df_freq_res = pd.DataFrame()
+    
+    for player in df_freq['player'].unique():
+        # Delete rows we don't want to see
+        # Define the strings to be deleted
+        strings_to_delete = ['palacio', 'bayerleverkusen','andrich','leverkusen', 'bayer', 'au', 'fur', 'nicht', 'diabi','alonso', 'wurd', 'hatt', 'zwei', 'seit', 'acht',
+                             'man', 'get', 'player','july', 's', 't', 'it',
+                             'ma', 'do', 'tambien', 'tra', 'xa']
+
+        # Delete rows containing the specified strings 
+        df_freq = df_freq[~df_freq['Word'].isin(strings_to_delete)]
+
+        # Select word frequencies for one Player
+        df_player = df_freq[(df_freq["player"] == player)]
+        # Print out the top 20 word frequencies 
+        print('Top Words for ', player , '\n', df_player.head(20), '\n')
+
+        # Add data to the DataFrame
+        df_freq_res  = pd.concat([df_freq_res, df_player.head(20)], axis=0)
+
+    
+    return df_freq_res
+
+
 def name_wordgroups(df):
-    '''
-    Function to match first and surname to just last name
-    '''
+    """
+    This function matches first and surname of players to just last name 
+    
+    Parameters:
+        df: DataFrame with the data in the column 'data'
+    
+    Returns:
+        DataFrame with all player names as just last names
+    """
     # create patterns which should be matched 
     # first lastname and firstname should both result in just lastname
     pattern_match2d = np.array([[r"\b(mitchel bakker|mitchel)\b", 'bakker'], 
